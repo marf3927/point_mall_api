406203814edbaac1a950b820111be657b07d65d1
marf3927 <xogml3927@naver.com>
2019-08-22 15:17:15 +0900

    database

--- point_mall/settings.py
+++ point_mall/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'point_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'point_mall',
+        'USER': 'root',
+        'PASSWORD': 'pointmallpwd',
+        'HOST': 'point-mall.cijixwpyzfq3.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
